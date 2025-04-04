ccbb39c1567e52e7d2e971ac7daa272b8af91a23
Sharmistha21 <sharmistha.chak21@gmail.com>
2025-04-04 23:40:09 +0530

    user login commit

--- Models/userSchema.js
+++ Models/userSchema.js
@@ -3,15 +3,20 @@ const mongoose=require('mongoose')
 const userSchema=new mongoose.Schema({
     username:{
         type:String,
-        required:true
+        required:true,
+        maxlength:[20,'username cant be more than 20 chars'],
+        unique:true
     },
     email:{
         type:String,
-        required:true
+        required:true,
+        unique:true
+
     },
     password:{
         type:String,
-        required: true
+        required: true,
+        minlength:[6,'password must at least be 6 chars']
     }
 })
 
